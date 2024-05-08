4a953451441a8cfa0bcf3b9055dec2130134d293
carrera7 <josuecarrera788@gmail.com>
2024-05-07 21:20:18 -0300

    agrego conexiones static

--- YateMate/YateMate/settings.py
+++ YateMate/YateMate/settings.py
@@ -56,7 +56,8 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'Home', 'templates')
+            os.path.join(BASE_DIR, 'Home', 'templates'),
+            os.path.join(BASE_DIR, 'templates')
             ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -118,8 +119,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # Añade 'static' como directorio de archivos estáticos
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
