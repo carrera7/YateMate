6778a1305cf51f7f1f62659237bba985a91cf7d3
carrera7 <josuecarrera788@gmail.com>
2024-05-08 22:46:41 -0300

    agrego funcionalidad de listado de objetos valiosos

--- YateMate/Trueque/views.py
+++ YateMate/Trueque/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from .models import Publicacion_ObjetoValioso
 
 def list_publication(request):
-     return render(request, "list_publication.html")
+    objetos_vigentes = Publicacion_ObjetoValioso.objects.filter(estado='Vigente')
+    return render(request, "list_publication.html", {'objetos_vigentes': objetos_vigentes})
