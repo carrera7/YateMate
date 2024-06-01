879a4deb174a4032f94035402a66bdc55bc0ff8e
carrera7 <josuecarrera788@gmail.com>
2024-06-01 13:33:13 -0300

    filtro de fechas en amarras

--- YateMate/Amarra/migrations/0001_initial.py
+++ YateMate/Amarra/migrations/0001_initial.py
@@ -4,25 +4,29 @@ import datetime
 from django.db import migrations, models
 import django.db.models.deletion
 
-
 def create_initial_amarras(apps, schema_editor):
     Publicacion_Amarra = apps.get_model('Amarra', 'Publicacion_Amarra')
+    User = apps.get_model('Register', 'User')
 
-    Publicacion_Amarra.objects.create(
-        dueño=1,
-        precio=100.00,
-        fecha_inicio=datetime.date(2024, 6, 1),
-        cant_dias=10,
-        ubicacion='Ubicación 1'
-    )
-    Publicacion_Amarra.objects.create(
-        dueño=2,
-        precio=200.00,
-        fecha_inicio=datetime.date(2024, 6, 15),
-        cant_dias=20,
-        ubicacion='Ubicación 2'
-    )
+    # Obtener instancias de usuario
+    user1 = User.objects.first()
+    user2 = User.objects.last()
 
+    if user1 and user2:
+        Publicacion_Amarra.objects.create(
+            dueño=user1,
+            precio="100.00",
+            fecha_inicio=datetime.date(2024, 6, 1),
+            cant_dias="10",
+            ubicacion='Ubicación 1'
+        )
+        Publicacion_Amarra.objects.create(
+            dueño=user2,
+            precio="200.00",
+            fecha_inicio=datetime.date(2024, 6, 15),
+            cant_dias="20",
+            ubicacion='Ubicación 2'
+        )
 
 class Migration(migrations.Migration):
 
@@ -37,9 +41,9 @@ class Migration(migrations.Migration):
             name='Publicacion_Amarra',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('precio', models.DecimalField(max_digits=10, decimal_places=2)),
+                ('precio', models.TextField()),
                 ('fecha_inicio', models.DateField()),
-                ('cant_dias', models.IntegerField()),
+                ('cant_dias', models.TextField()),
                 ('ubicacion', models.TextField()),
                 ('dueño', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='Register.user')),
             ],
--- YateMate/Amarra/views.py
+++ YateMate/Amarra/views.py
@@ -4,11 +4,28 @@ from .forms import AmarraForm
 from django.contrib import messages
 from .models import Publicacion_Amarra
 from Register.models import User
+from datetime import datetime, timedelta
 
 def list_amarra(request):
-    #obtengo todos los alquileres 
-    #sino   
-    Amarras = Publicacion_Amarra.objects.all()       
+    if request.method == 'GET' and 'fecha_inicio' in request.GET and 'fecha_fin' in request.GET:
+        fecha_inicio = request.GET.get('fecha_inicio')
+        fecha_fin = request.GET.get('fecha_fin')
+        try:
+            fecha_inicio = datetime.strptime(fecha_inicio, '%Y-%m-%d').date()
+            fecha_fin = datetime.strptime(fecha_fin, '%Y-%m-%d').date()
+        except ValueError:
+            messages.error(request, 'Formato de fecha inválido.')
+            return redirect('list_amarra')
+        
+        # Calcular la fecha de fin sumando la cantidad de días a la fecha de inicio
+        dias = (fecha_fin - fecha_inicio).days
+        Amarras = Publicacion_Amarra.objects.filter(
+            fecha_inicio__gte=fecha_inicio,
+            fecha_inicio__lte=fecha_inicio + timedelta(days=dias)
+        )
+    else:
+        Amarras = Publicacion_Amarra.objects.all()
+
     return render(request, "list_amarra.html", {'objetos': Amarras})
 
 
