b240136542b6d81e145a58f900966f4618ee1773
CaroSchwab97 <carolina.schwab190496@alumnos.info.unlp.edu.ar>
2024-06-06 21:58:40 -0300

    cambios del form de register.html

--- YateMate/Home/froms.py
+++ YateMate/Home/froms.py
@@ -1,5 +1,5 @@
 from datetime import date
-import re 
+import re
 from django import forms
 from django.core.exceptions import ValidationError
 from django.core.validators import EmailValidator, MinLengthValidator, RegexValidator
@@ -7,11 +7,17 @@ import dns
 import dns.resolver
 from Register.models import User
 
+
 class CustomUserRegistrationForm(forms.ModelForm):
-    
+    mail = forms.CharField(label='Correo Electrónico',
+                           validators=[EmailValidator(message='Por favor, introduce un correo electrónico válido.')],
+                           error_messages={'required': 'Este campo es obligatorio',
+                                           'invalid': 'Por favor, introduce un correo electrónico válido'})
+
     class Meta:
         model = User
-        fields = ['mail', 'password', 'nombre', 'apellido', 'dni', 'fecha_nacimiento', 'fecha_expiracion_dni', 'nacionalidad', 'genero', 'domicilio', 'cuil_cuit']
+        fields = ['mail', 'password', 'nombre', 'apellido', 'dni', 'fecha_nacimiento', 'fecha_expiracion_dni',
+                  'nacionalidad', 'genero', 'domicilio', 'cuil_cuit']
         labels = {
             'nombre': 'Nombre',
             'apellido': 'Apellido',
@@ -31,39 +37,24 @@ class CustomUserRegistrationForm(forms.ModelForm):
             'fecha_nacimiento': 'Formato: DD-MM-AAAA. Ejemplo: 01/02/1990',
             'password': 'La contraseña debe contener al menos una letra mayúscula, una letra minúscula, un número y un carácter especial de la lista @$!%*?&. Ejemplo: P@ssw0rd!',
         }
-        error_messages = {
-            'dni': {
-                'min_length': 'El DNI debe contener al menos 7 caracteres.',
-                'max_length': 'El DNI no puede contener más de 8 caracteres.',
-                'invalid': 'El DNI solo puede contener números.',
-            },
-            'cuil_cuit': {
-                'min_length': 'El CUIL/CUIT debe contener al menos 11 caracteres.',
-                'max_length': 'El CUIL/CUIT no puede contener más de 13 caracteres.',
-                'invalid': 'El CUIL/CUIT solo puede contener números y guiones.',
-            },
-        }
-       
+
     password = forms.CharField(
-    label='Contraseña',
-    widget=forms.PasswordInput,
-    validators=[
-        MinLengthValidator(8),
-        # Validación personalizada para la contraseña
-        RegexValidator(
-            regex=r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]+$',
-            message='La contraseña debe contener al menos una letra mayúscula, una letra minúscula, un número y un carácter especial.'
-        )
-    ],
-    help_text='La contraseña debe contener al menos una letra mayúscula, una letra minúscula, un número y un carácter especial.'
-)
-    
-    mail = forms.CharField(label='Correo Electrónico', validators=[EmailValidator(message='Por favor, introduce un correo electrónico válido.')])
-    
+        label='Contraseña',
+        widget=forms.PasswordInput,
+        validators=[
+            MinLengthValidator(8),
+            # Validación personalizada para la contraseña
+            RegexValidator(
+                regex=r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]+$',
+                message='La contraseña debe contener al menos una letra mayúscula, una letra minúscula, un número y un carácter especial.'
+            )
+        ],
+        help_text='La contraseña debe contener al menos una letra mayúscula, una letra minúscula, un número y un carácter especial.'
+    )
 
     def clean_mail(self):
         mail = self.cleaned_data['mail']
-        
+
         try:
             domain = mail.split('@')[1]
             mx_records = dns.resolver.resolve(domain, 'MX')
@@ -97,18 +88,8 @@ class CustomUserRegistrationForm(forms.ModelForm):
         fecha_nacimiento = self.cleaned_data.get('fecha_nacimiento')
         if fecha_nacimiento:
             today = date.today()
-            edad = today.year - fecha_nacimiento.year - ((today.month, today.day) < (fecha_nacimiento.month, fecha_nacimiento.day))
+            edad = today.year - fecha_nacimiento.year - (
+                    (today.month, today.day) < (fecha_nacimiento.month, fecha_nacimiento.day))
             if edad < 18:
                 raise ValidationError("Debes ser mayor de edad para registrarte.")
-            return fecha_nacimiento  # Devuelve la fecha de nacimiento validada
-        return None
-
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        user.tipo = 'Usuario'
-        if commit:
-            user.save()
-        return user
-    
-    field_order = ['nombre', 'apellido', 'dni', 'fecha_nacimiento', 'fecha_expiracion_dni', 'nacionalidad', 'genero', 'domicilio', 'cuil_cuit', 'mail', 'password']
-
+            return fecha_nacimiento
