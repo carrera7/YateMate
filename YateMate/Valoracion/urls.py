136da36d3525963985c2495d9150e1c85dc85563
CaroSchwab97 <carolina.schwab190496@alumnos.info.unlp.edu.ar>
2024-07-07 19:34:19 -0300

    arreglo error

--- YateMate/Valoracion/urls.py
+++ YateMate/Valoracion/urls.py
@@ -19,5 +19,6 @@ urlpatterns = [
 
     path('valoraciones_de_usuarios/', views.listar_valoraciones_de_usuarios, name='valoraciones_de_usuarios'),
     path('ver_valoraciones_admin/', views.ver_valoraciones_admin, name="ver_valoraciones_admin"),
-    path('valoraciones/<int:usuario_id>/', views.ver_valoraciones_usuario_admin, name='ver_valoraciones_usuario'),
+    path('ver_valoraciones_usuario/<int:usuario_id>/', views.ver_valoraciones_usuario, name='ver_valoraciones_usuario'),
+    path('valoraciones/<int:usuario_id>/', views.ver_valoraciones_usuario_admin, name='ver_valoraciones_usuario_admin'),
 ]
